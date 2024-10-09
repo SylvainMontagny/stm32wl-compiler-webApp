456ed529bd734e96ee43923eb95a649817553d81
elias-qzo <eliascuzeau@gmail.com>
2024-10-10 00:32:48 +0200

    Templates + function guideline

--- docker/dockerfunctions.js
+++ docker/dockerfunctions.js
@@ -2,6 +2,26 @@ const Docker = require('dockerode');
 const docker = new Docker();
 var stream = require('stream');
 
+jsonTest = ["ACTIVATION_MODE" = "OTAA","CLASS" = "CLASS_A"]
+
+//libraire fs
+function compile(jsonConfig){
+    //1.Generer un id random
+    // id = random(15)
+    // id = 4382592
+
+    //2.creer configs/4382592 et results/4382592
+
+    //3.creer une copie de General_Setup_template.h et application_config_template.h dans configs/4382592
+
+    //4.modifie les deux avec les paramètres du json
+    //format du json [ "ACTIVATION_MODE" = "OTAA", ...]
+    //ouvrir le fichier template, faire regex de {KEY} -> value
+
+    //5. enlever _template dans le nom
+
+    //6. appeler la fonction startCompiler(id) (pas pour le moment)
+}
 
 // Display container logs on console.log
 function containerLogs(container) {
