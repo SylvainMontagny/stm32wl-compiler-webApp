93d9a40c9fc174cad736243d28588f697a7b6082
Maxime272003 <62075092+Maxime272003@users.noreply.github.com>
2024-10-10 12:47:15 +0200

    interface html css + advcanced js

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello world !");
+// Display advanced settings form
+let advancedContainer = document.querySelector('.advance-container .title-container');
+advancedContainer.addEventListener('click', function() {
+    let advancedForm = document.querySelector('.advanced-settings-form');
+    let restore = document.querySelector('.advance-container .title-container .restore');
+    let closed = document.querySelector('.closed-advance-container p');
+
+    if (advancedForm.style.display === '' || advancedForm.style.display === 'none') {
+        advancedForm.style.display = 'grid'; 
+        restore.style.display = 'block';     
+        closed.style.display = 'none';       
+    } else {
+        advancedForm.style.display = 'none'; 
+        restore.style.display = 'none';      
+        closed.style.display = 'block';     
+    }
+});
