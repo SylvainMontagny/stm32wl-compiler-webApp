433b4ce0134c40a0fad1c3b10669b169f13fb596
Maxime272003 <62075092+Maxime272003@users.noreply.github.com>
2024-10-17 10:51:31 +0200

    random key gen

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 // Display advanced settings form
-let advancedContainer = document.querySelector('.advance-container .title-container');
+let advancedContainer = document.querySelector('.advance-container .title-container .closed-advance-container');
 let svg_arrow = document.querySelector('.advance-container .title-container .closed-advance-container .svg');
 advancedContainer.addEventListener('click', function() {
     let advancedForm = document.querySelector('.advanced-settings-form');
@@ -117,7 +117,7 @@ cayenne_2.addEventListener('change', cayenne2Error);
 let sim_on = document.getElementById('mrl003-sim-on');
 let sim_off = document.getElementById('mrl003-sim-off');
 
-sim_on.addEventListener('change', function() {
+function sim_onError() {
     hello.disabled = true;
     hello_label.style.color = '#D1D1D1';
     temperature.disabled = true;
@@ -133,9 +133,12 @@ sim_on.addEventListener('change', function() {
     humidity.checked = false;
     cayenne_1.checked = false;
     cayenne_2.checked = true;
-});
+}
 
-sim_off.addEventListener('change', function() {
+sim_on.addEventListener('change', sim_onError);
+
+
+function sim_offError() {
     hello.disabled = false;
     hello_label.style.color = '#000';
     temperature.disabled = false;
@@ -146,7 +149,9 @@ sim_off.addEventListener('change', function() {
     cayenne_2_label.style.color = '#000';
     temperature_label.style.color = '#000';
     humidity_label.style.color = '#000';
-});
+}
+
+sim_off.addEventListener('change', sim_offError);
 
 
 // Restore default settings
@@ -172,4 +177,67 @@ r_app.addEventListener('click', function() {
     document.getElementById('cayenne-lpp-disabled').checked = true;
     document.getElementById('hello').checked = true;
     document.getElementById('low-power-disabled').checked = true;
+});
+
+// restore advance settings
+let r_advance = document.getElementById('restore-adv');
+r_advance.addEventListener('click', function() {
+    document.getElementById('admin-sensor-disabled').checked = true;
+    document.getElementById('mrl003-app-port').value = '30';
+    document.getElementById('mrl003-sim-off').checked = true;
+    sim_offError();
+});
+
+
+
+// generate random credentials
+const genRandomKey = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+let generate_deveui = document.getElementById('generate-dev-eui');
+let generate_appKey = document.getElementById('generate-appkey');
+let generate_appEUI = document.getElementById('generate-appeui');
+let generate_devAddr = document.getElementById('generate-devaddr');
+let generate_nwkskey = document.getElementById('generate-nwkskey');
+let generate_appskey = document.getElementById('generate-appskey');
+let generate_adminAppKey = document.getElementById('generate-admin-gen-app-key');
+
+// generate random DevEUI
+document.getElementById('dev-eui').value = genRandomKey(16);
+generate_deveui.addEventListener('click', function() {
+    document.getElementById('dev-eui').value = genRandomKey(16);
+});
+
+// generate random appKey
+document.getElementById('appkey').value = genRandomKey(32);
+generate_appKey.addEventListener('click', function() {
+    document.getElementById('appkey').value = genRandomKey(32);
+});
+
+// generate random appEUI
+document.getElementById('appeui').value = genRandomKey(16);
+generate_appEUI.addEventListener('click', function() {
+    document.getElementById('appeui').value = genRandomKey(16);
+});
+
+// generate random devAddr
+document.getElementById('devaddr').value = genRandomKey(8);
+generate_devAddr.addEventListener('click', function() {
+    document.getElementById('devaddr').value = genRandomKey(8);
+});
+
+// generate random NwkSKey
+document.getElementById('nwkskey').value = genRandomKey(32);
+generate_nwkskey.addEventListener('click', function() {
+    document.getElementById('nwkskey').value = genRandomKey(32);
+});
+
+// generate random AppSKey
+document.getElementById('appskey').value = genRandomKey(32);
+generate_appskey.addEventListener('click', function() {
+    document.getElementById('appskey').value = genRandomKey(32);
+});
+
+// generate random adminAppKey
+document.getElementById('admin-gen-app-key').value = genRandomKey(32);
+generate_adminAppKey.addEventListener('click', function() {
+    document.getElementById('admin-gen-app-key').value = genRandomKey(32);
 });
