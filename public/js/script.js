42f300d86bc8bef9fe8c2066a7364c346ae920d6
Maxime272003 <rousseaumaxime27@gmail.com>
2024-10-20 12:56:23 +0200

    small fix

--- public/js/script.js
+++ public/js/script.js
@@ -37,6 +37,9 @@ const elements = {
     adminAppKey: document.getElementById('admin-gen-app-key'),
     mrlAppPort: document.getElementById('mrl003-app-port'),
     generateFirmware: document.getElementById('generate-firmware'),
+    class : document.getElementById('class'),
+    spreadingFactor : document.getElementById('spreading-factor'),
+    appPort : document.getElementById('app_port'),
 };
 
 // Display advanced settings form
@@ -171,11 +174,11 @@ elements.simOff.addEventListener('change', simOffError);
 // Restore default settings
 elements.rLorawan.addEventListener('click', function() {
     elements.activationMode.value = 'otaa';
-    document.getElementById('class').value = 'class_a';
-    document.getElementById('spreading-factor').value = '7';
+    elements.class.value = 'class_a';
+    elements.spreadingFactor.value = '7';
     document.getElementById('adaptative-dr-off').checked = true;
     document.getElementById('confirmation-off').checked = true;
-    document.getElementById('app_port').value = '15';
+    elements.appPort.value = '15';
     otaaAbp();
     saveFormData();
 });
@@ -194,71 +197,62 @@ elements.rApp.addEventListener('click', function() {
 elements.rAdvance.addEventListener('click', function() {
     document.getElementById('admin-sensor-disabled').checked = true;
     elements.mrlAppPort.value = '30';
+    if(elements.simOn.checked) {
+        simOffError();
+        elements.hello.checked = true;
+        helloError();
+    }
     elements.simOff.checked = true;
-    simOffError();
     saveFormData();
 });
 
 // Generate random credentials
-const genRandomKey = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-
-window.onload = function() {
-    // Générer des clés aléatoires au chargement initial de la page
-    elements.devEui.value = genRandomKey(16);
-    elements.appKey.value = genRandomKey(32);
-    elements.appEui.value = genRandomKey(16);
-    elements.devAddr.value = genRandomKey(8);
-    elements.nwksKey.value = genRandomKey(32);
-    elements.appsKey.value = genRandomKey(32);
-    elements.adminAppKey.value = genRandomKey(32);
-
-    // Attacher les événements de clic pour générer de nouvelles clés
-    elements.generateDevEui.addEventListener('click', function() {
-        elements.devEui.value = genRandomKey(16);
-        saveFormData();
-    });
-
-    elements.generateAppKey.addEventListener('click', function() {
-        elements.appKey.value = genRandomKey(32);
-        saveFormData();
-    });
-
-    elements.generateAppEUI.addEventListener('click', function() {
-        elements.appEui.value = genRandomKey(16);
-        saveFormData();
-    });
-
-    elements.generateDevAddr.addEventListener('click', function() {
-        elements.devAddr.value = genRandomKey(8);
-        saveFormData();
-    });
-
-    elements.generateNwkskey.addEventListener('click', function() {
-        elements.nwksKey.value = genRandomKey(32);
-        saveFormData();
-    });
-
-    elements.generateAppskey.addEventListener('click', function() {
-        elements.appsKey.value = genRandomKey(32);
-        saveFormData();
-    });
-
-    elements.generateAdminAppKey.addEventListener('click', function() {
-        elements.adminAppKey.value = genRandomKey(32);
-        saveFormData();
-    });
+const genRandomKey = (size, element) => {
+    const key = [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+    element.value = key;
+    saveFormData(); // saveFormData() after generating a new key
+    return key;
 };
+
+elements.generateDevEui.addEventListener('click', function() {
+    genRandomKey(16, elements.devEui);
+});
+
+elements.generateAppKey.addEventListener('click', function() {
+    genRandomKey(32, elements.appKey);
+});
+
+elements.generateAppEUI.addEventListener('click', function() {
+    genRandomKey(16, elements.appEui);
+});
+
+elements.generateDevAddr.addEventListener('click', function() {
+    genRandomKey(8, elements.devAddr);
+});
+
+elements.generateNwkskey.addEventListener('click', function() {
+    genRandomKey(32, elements.nwksKey);
+});
+
+elements.generateAppskey.addEventListener('click', function() {
+    genRandomKey(32, elements.appsKey);
+});
+
+elements.generateAdminAppKey.addEventListener('click', function() {
+    genRandomKey(32, elements.adminAppKey);
+});
+
 
 
 // Save form data to localStorage
 function saveFormData() {
     const formData = {
         activationMode: elements.activationMode.value,
-        class: document.getElementById('class').value,
-        spreadingFactor: document.getElementById('spreading-factor').value,
+        class: elements.class.value,
+        spreadingFactor: elements.spreadingFactor.value,
         adaptativeDr: document.querySelector('input[name="adaptative-dr"]:checked').value,
         confirmation: document.querySelector('input[name="confirmation"]:checked').value,
-        appPort: document.getElementById('app_port').value,
+        appPort: elements.appPort.value,
         sendMode: document.querySelector('input[name="send-mode"]:checked').value,
         frameDelay: document.getElementById('frame-delay').value,
         hello: elements.hello.checked,
@@ -286,11 +280,11 @@ function restoreFormData() {
         const formData = JSON.parse(savedData);
 
         elements.activationMode.value = formData.activationMode || 'otaa';
-        document.getElementById('class').value = formData.class || 'class-a';
-        document.getElementById('spreading-factor').value = formData.spreadingFactor || 'sf7';
+        elements.class.value = formData.class || 'class_a';
+        elements.spreadingFactor.value = formData.spreadingFactor || '7';
         document.querySelector(`input[name="adaptative-dr"][value="${formData.adaptativeDr || 'off'}"]`).checked = true;
         document.querySelector(`input[name="confirmation"][value="${formData.confirmation || 'off'}"]`).checked = true;
-        document.getElementById('app_port').value = formData.appPort || '15';
+        elements.appPort.value = formData.appPort || '15';
         document.querySelector(`input[name="send-mode"][value="${formData.sendMode || 'every-frame-delay'}"]`).checked = true;
         document.getElementById('frame-delay').value = formData.frameDelay || '10';
         elements.hello.checked = formData.hello || false;
@@ -298,13 +292,13 @@ function restoreFormData() {
         elements.humidity.checked = formData.humidity || false;
         document.querySelector(`input[name="low-power"][value="${formData.lowPower || 'disabled'}"]`).checked = true;
         document.querySelector(`input[name="cayenne-lpp"][value="${formData.cayenneLpp || 'disabled'}"]`).checked = true;
-        elements.devEui.value = formData.devEui;
-        elements.appKey.value = formData.appKey;
-        elements.appEui.value = formData.appEui;
-        elements.devAddr.value = formData.devAddr;
-        elements.nwksKey.value = formData.nwkSKey;
-        elements.appsKey.value = formData.appSKey;
-        elements.adminAppKey.value = formData.adminAppKey;
+        elements.devEui.value = formData.devEui || genRandomKey(16, elements.devEui);
+        elements.appKey.value = formData.appKey || genRandomKey(32, elements.appKey);
+        elements.appEui.value = formData.appEui || genRandomKey(16, elements.appEui);
+        elements.devAddr.value = formData.devAddr || genRandomKey(8, elements.devAddr);
+        elements.nwksKey.value = formData.nwkSKey || genRandomKey(32, elements.nwksKey);
+        elements.appsKey.value = formData.appSKey || genRandomKey(32, elements.appsKey);
+        elements.adminAppKey.value = formData.adminAppKey || genRandomKey(32, elements.adminAppKey);
         document.querySelector(`input[name="mrl003-sim"][value="${formData.mrlSim || 'off'}"]`).checked = true;
         elements.mrlAppPort.value = formData.mrlAppPort || '30';
     }
@@ -333,11 +327,11 @@ function formatKey(str){
 function getFormJsonString() {
     let formData = {
         ACTIVATION_MODE: elements.activationMode.value.toUpperCase(),
-        CLASS: document.getElementById('class').value.toUpperCase(),
-        SPREADING_FACTOR: document.getElementById('spreading-factor').value.toUpperCase(),
+        CLASS: elements.class.value.toUpperCase(),
+        SPREADING_FACTOR: elements.spreadingFactor.value.toUpperCase(),
         ADAPTIVE_DR: (document.querySelector('input[name="adaptative-dr"]:checked').value == 'on').toString(),
         CONFIRMED: (document.querySelector('input[name="confirmation"]:checked').value.toString() == 'on').toString(),
-        APP_PORT: document.getElementById('app_port').value,
+        APP_PORT: elements.appPort.value,
         SEND_BY_PUSH_BUTTON: (document.querySelector('input[name="send-mode"]:checked').value == 'push-button').toString(),
         FRAME_DELAY: document.getElementById('frame-delay').value * 1000,
         PAYLOAD_HELLO: elements.hello.checked.toString(),
@@ -360,12 +354,37 @@ function getFormJsonString() {
     return JSON.stringify(formData, null, 2);
 }
 
+function validateForm() {
+    let isValid = true;
+    const inputs = [
+        { element: elements.appPort, min: 0, max: 255, name: 'App Port' },
+        { element: document.getElementById('frame-delay'), min: 8, max: 100, name: 'Frame Delay' },
+        { element: elements.mrlAppPort, min: 1, max: 255, name: 'MRL003 App Port' }
+    ];
+
+    inputs.forEach(input => {
+        const value = parseInt(input.element.value);
+        if (isNaN(value) || value < input.min || value > input.max) {
+            isValid = false;
+            alert(`${input.name} must be a number between ${input.min} and ${input.max}`);
+        }
+    });
+
+    return isValid;
+}
+
+
 // Button to compile
 document.getElementById('generate-firmware').addEventListener('click', function() {
-    let jsonString = getFormJsonString();
-    console.log(jsonString);
-    compileFirmware(jsonString);
-} );
+    if (validateForm()) {
+        let jsonString = getFormJsonString();
+        console.log(jsonString);
+        // compileFirmware(jsonString); 
+    } else {
+        alert('Please fix the errors in the form before compiling the firmware');
+    }
+});
+
 
 // function compile firmware from jsonString of all form data
 async function compileFirmware(jsonString){
